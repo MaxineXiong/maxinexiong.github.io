66e11b6308f2de38d9975bbf4d65663be6198a14
MaxineXiong <maxinexiong2@gmail.com>
2024-04-02 00:17:17 +1100

    UiPath Calendar Picker Bot added

--- light-mode/assets/js/main.js
+++ light-mode/assets/js/main.js
@@ -318,7 +318,9 @@ const projects = [
 																																														            'IP converter', 'IP Lookup', 'Data Scraping', 'Excel Output', 'Writing into Excel', 'Write into Excel',
 																																																			  'Excel population', 'populating excel', 'populate excel', 'excel sheet population'] },
 	{ name: "Image Background Remover Bot", url: "./projects/rpa.html#rpa-bg-remover", labels: ['UiPath RPA', 'Robotic Automation Process', 'UiPath Robot', 'UiPath Studio', 'RPA Workflow', 'Automation Processes', 'UiPath Modern Design',
-																																															'remove background', 'remove image background', 'image background removal', 'file handling', 'move files'] }
+																																															'remove background', 'remove image background', 'image background removal', 'file handling', 'move files'] },
+	{ name: "Calendar Picker Bot", url: "./projects/rpa.html#rpa-bg-remover", labels: ['UiPath RPA', 'Robotic Automation Process', 'UiPath Robot', 'UiPath Studio', 'RPA Workflow', 'Automation Processes', 'UiPath Modern Design',
+																																										 'pick dates', 'dates picker', 'check-in date', 'check-out date', 'booking.com', 'twerk selectors', 'fine-tune selectors'] }
 	// Add more projects here
 ];
 
